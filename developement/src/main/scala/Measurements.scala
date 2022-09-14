c975489797df401ff4e538ded782f4f4ab2bd161
vijay <vijay.vaswani@ccsmed.com>
2022-09-14 10:36:35 +0530

    initial commit

---
+++ developement/src/main/scala/Measurements.scala
@@ -0,0 +1,7 @@
+/*
+* Author :  Vijay Vaswani
+* Date   :  14-09-2022
+* Project:  developement
+* */
+
+case class Measurements(var processedFile:Int, var processedMeasurements:Int,var failedMeasurements:Int)
